e61107a9a1dc476a04b826c7de5b28e68546e2d5
AshishKotnalaFS <kotnalaashish2112@gmail.com>
2020-08-13 13:14:45 +0530

    ashish

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get("/api/:id", (req, res) => {
     res.send(requireddata);
   });
 });
+
 app.listen(port, () => console.log(`listening on port ${port}...`));
 function main() {
   let result1;
